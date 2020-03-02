d68fc426b3484ded6eb5f98205d79225eee43387
2flo <dianeflozin@gmail.com>
2020-03-02 11:54:33 +0100

    Nouvelle tentative de push

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var todoController = require('./Controllers/todoController');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use('./assets', express.static('./public'));
+
+todoController(app);
+app.listen(3000);
+console.log('Welcome in the port 3000');
