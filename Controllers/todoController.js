0c39228064f452ae357744761c3cff7a7574cdad
2flo <dianeflozin@gmail.com>
2020-03-11 17:17:41 +0100

    Ajout du middleware + ajout de la fonction ajouter une nouvelle tâche mais problème cela m'affiche en format json

--- Controllers/todoController.js
+++ Controllers/todoController.js
@@ -1,9 +1,14 @@
+var bodyParser =require('body-parser');
+
+var data = [{item: 'Ecouter la compagnie créole'}, {item: 'Apprendre Symfony'}, {item: 'Créer un site perso'}]
+var urlencodedParser = bodyParser.urlencoded({extended: true});
 module.exports = function(app){
     app.get('/todo', function(req, res){
-      res.render('todo');
+      res.render('todo', {todos: data});
     });
-    app.post('/todo', function(req, res){
-
+    app.post('/todo', urlencodedParser, function(req, res){
+      data.push(req.body);
+      res.json({todos: data});
     });
     app.delete('/todo', function(req, res){
 
