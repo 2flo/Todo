efc7a0cf3134a6249f2fdfe2c83c0c554e05f201
2flo <dianeflozin@gmail.com>
2020-03-12 16:08:07 +0100

    Modification dans le controller: remplacement du json() par write()

--- Controllers/todoController.js
+++ Controllers/todoController.js
@@ -1,15 +1,18 @@
 var bodyParser =require('body-parser');
 
 var data = [{item: 'Ecouter la compagnie créole'}, {item: 'Apprendre Symfony'}, {item: 'Créer un site perso'}]
-var urlencodedParser = bodyParser.urlencoded({extended: true});
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
     app.get('/todo', function(req, res){
       res.render('todo', {todos: data});
     });
+
     app.post('/todo', urlencodedParser, function(req, res){
       data.push(req.body);
-      res.json({todos: data});
+      res.write({todos: data});
     });
+
     app.delete('/todo', function(req, res){
 
     });
