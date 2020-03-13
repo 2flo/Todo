304192f886992018e223d46e1df95ab30ae25453
2flo <dianeflozin@gmail.com>
2020-03-13 17:23:00 +0100

    Nouvelle tentative de modification de la reponse json

--- Controllers/todoController.js
+++ Controllers/todoController.js
@@ -1,4 +1,4 @@
-var bodyParser =require('body-parser');
+var bodyParser = require('body-parser');
 
 var data = [{item: 'Ecouter la compagnie créole'}, {item: 'Apprendre Symfony'}, {item: 'Créer un site perso'}]
 var urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -10,7 +10,7 @@ module.exports = function(app){
 
     app.post('/todo', urlencodedParser, function(req, res){
       data.push(req.body);
-      res.write({todos: data});
+      res.send({todos: data});
     });
 
     app.delete('/todo', function(req, res){
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var todoController = require('./Controllers/todoController');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.use( express.static('./public'), bodyParser.urlencoded());
+app.use( express.static('./public'));
 
 todoController(app);
 app.listen(3000);
