4c3f0efacd4f3d132f5a7918c6c4ba0e8d4f031e
2flo <dianeflozin@gmail.com>
2020-03-18 16:08:06 +0100

    Ajout d'une propriété de suppression dans la to-do liste

--- assets/todo-list.js
+++ assets/todo-list.js
@@ -12,5 +12,16 @@ $(document).ready(function(){
             }
         });
         return false;
+    });
+
+    $('li').on('click', function(){
+        var item = $(this).text().replace(/ /g, "-");
+        $.ajax({
+            type: 'DELETE',
+            url: '/todo/' + item,
+            success: function(data){
+                location.reload();
+            }
+        })
     })
 })
