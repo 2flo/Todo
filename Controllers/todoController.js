844b646db727281c00f26421160b02e568652e2d
2flo <dianeflozin@gmail.com>
2020-04-01 17:58:37 +0200

    Connexion du projet à la base de données mongoDB et réécriture du code pour l'adapter à mongoDB

--- Controllers/todoController.js
+++ Controllers/todoController.js
@@ -1,34 +1,35 @@
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose', {useUnifiedTopology: true});
+var mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://test:123@cluster0-lhrvg.gcp.mongodb.net/test', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://floriane_dazin:Kl@in6Forever@cluster0-lhrvg.gcp.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
 
 var todoSchema = new mongoose.Schema({
   item: String
 });
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = Todo({item: 'Sortir le pain du four'}).save(function(err){
-  if (err) throw err;
-  console.log('item saved');
-});
 
-var data = [{item: 'Ecouter la compagnie créole'}, {item: 'Apprendre Symfony'}, {item: 'Créer un site perso'}]
+//var data = [{item: 'Ecouter la compagnie créole'}, {item: 'Apprendre Symfony'}, {item: 'Créer un site perso'}]
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
     app.get('/todo', function(req, res){
-      res.render('todo', {todos: data});
+      Todo.find({}, function(err, data){
+        if (err) throw err;      
+        res.render('todo', {todos: data});
+      });
     });
 
     app.post('/todo', urlencodedParser, function(req, res){
-      data.push(req.body);
-      res.send({todos: data});
+      var newTodo = Todo(req.body).save(function(err, data){
+        if (err) throw err;
+        res.json(data);
+      });
     });
 
     app.delete('/todo/:item', function(req, res){
-      data = data.filter(function(todo){
-        return todo.item.replace(/ /g, "-") !== req.params.item;
+      Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+        if (err) throw err;
+        res.json(data);
       });
-      res.send(data);
     });
 };
